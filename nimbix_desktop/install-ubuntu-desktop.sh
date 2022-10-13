7324874f047bbc4d47b2289d6d46e53477e4a043
oxedions <oxedions@gmail.com>
2022-10-13 13:31:38 -0400

    Fix - tag 2.4 and verbosity

--- nimbix_desktop/install-ubuntu-desktop.sh
+++ nimbix_desktop/install-ubuntu-desktop.sh
@@ -22,7 +22,7 @@ if [ "$VERSION_ID" == "22.04" ]; then
     PKGS+=" libxtst6"
 fi
 
-apt-get -y install $PKGS #--no-install-recommends
+apt-get -y install $PKGS $RIS
 apt-get -y remove light-locker
 
 if [[ "$ARCH" != "x86_64" ]]; then
@@ -37,7 +37,7 @@ fi
 PY2=$(python -V 2>&1 |grep "^Python 2" || true)
 if [[ -n "$PY2" ]]; then
     # this clobbers py3 only, so do it only if we have py2
-    apt-get -y install python-pip libmagickwand-dev python-gtk2 python-gnome2 python-numpy #--no-install-recommends
+    apt-get -y install python-pip libmagickwand-dev python-gtk2 python-gnome2 python-numpy $RIS
     # Wand is used for screenshots
     pip install Wand
 fi
