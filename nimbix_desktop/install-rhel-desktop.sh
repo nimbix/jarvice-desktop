d396b57f569481db98206c6820dc93f512b21d21
oxedions <oxedions@gmail.com>
2023-02-23 20:02:48 -0500

    JAR-6790

--- nimbix_desktop/install-rhel-desktop.sh
+++ nimbix_desktop/install-rhel-desktop.sh
@@ -12,16 +12,19 @@ if [[ "${VERSION_ID:0:1}" == "7" ]]; then
        xorg-x11-fonts-Type1 xorg-x11-fonts-misc xorg-x11-fonts-75dpi xorg-x11-fonts-100dpi \
        xorg-x11-fonts-ISO8859-1-100dpi xorg-x11-fonts-ISO8859-1-75dpi \
        xkeyboard-config xorg-x11-apps xcb-util xcb-util-keysyms xorg-x11-utils \
-       net-tools glx-utils ImageMagick-devel firefox \
+       net-tools glx-utils ImageMagick-devel firefox xorg-x11-apps \
        ristretto xterm numpy python36-numpy python36-gobject python-pip
 elif [[ "${VERSION_ID:0:1}" == "8" ]]; then
+    dnf install 'dnf-command(config-manager)' -y
+    dnf install dnf-plugins-core -y
+    dnf config-manager --set-enabled powertools
     dnf -y groupinstall Xfce --nobest
     dnf -y install perl wget xauth pygtk2 adwaita-icon-theme  \
        xorg-x11-fonts-Type1 xorg-x11-fonts-misc xorg-x11-fonts-75dpi xorg-x11-fonts-100dpi \
        xorg-x11-fonts-ISO8859-1-100dpi xorg-x11-fonts-ISO8859-1-75dpi \
        xkeyboard-config xterm xcb-util xcb-util-keysyms xorg-x11-utils \
        net-tools glx-utils ImageMagick-devel firefox \
-       ristretto xterm python3-numpy python3-gobject python3-pip $RIS
+       ristretto xterm python3-numpy python3-gobject python3-pip xorg-x11-apps libGLU $RIS
     # Remove power manager to prevent pannel plugin crash at startup
     dnf -y remove xfce4-power-manager
 fi
